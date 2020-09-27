2d90358abed7d67dd6a4ce4f00e827be5f534826
Oleg Shipitsyn <olegshipitsyn.work@gmail.com>
2020-09-27 17:59:52 +0300

    existing check added

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -5,4 +5,10 @@ const sliderOptions_DEFAULT = {
     focusAt: 'center'
 }
 
-new Glide('.glide', sliderOptions_DEFAULT).mount()
+const isExist = ( elem ) => document.querySelector(elem)
+
+ if ( isExist('.glide') ){
+
+    new Glide('.glide', sliderOptions_DEFAULT).mount()
+    
+ }
